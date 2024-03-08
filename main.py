a12f13b44fb9734463778426a9e59651243de68a
Viet Bui <buiminhviet.2002@gmail.com>
2024-03-08 17:11:30 -0600

    enforce login require

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user
- 
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SECRET_KEY"] = "abc"
 db = SQLAlchemy()
- 
+
 login_manager = LoginManager()
 login_manager.init_app(app)
- 
- 
+
+
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
- 
- 
+
+
 db.init_app(app)
- 
- 
+
+
 with app.app_context():
     db.create_all()
- 
- 
-@login_manager.user_loader # this one has current user populate to home.html
+
+
+@login_manager.user_loader  # this one has current user populate to home.html
 def loader_user(user_id):
     return Users.query.get(user_id)
- 
- 
+
+
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -38,8 +38,14 @@ def register():
         db.session.commit()
         return redirect(url_for("login"))
     return render_template("signup.html")
- 
- 
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.username)
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -49,17 +55,19 @@ def login():
             login_user(user)
             return redirect(url_for("home"))
     return render_template("login.html")
- 
- 
+
+
 @app.route("/logout")
+@login_required
 def logout():
     logout_user()
     return redirect(url_for("home"))
- 
- 
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
- 
+
+
 if __name__ == "__main__":
     app.run()
