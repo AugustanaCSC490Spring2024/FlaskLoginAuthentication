d7ac2254022f91867f5cf20c7ff62a1f5947dce6
Viet Bui <buiminhviet.2002@gmail.com>
2024-03-09 13:25:57 -0600

    added hash into password

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
@@ -33,7 +34,7 @@ def loader_user(user_id):
 def register():
     if request.method == "POST":
         user = Users(username=request.form.get("username"),
-                     password=request.form.get("password"))
+                     password=generate_password_hash(request.form.get("password"), method='scrypt'))
         db.session.add(user)
         db.session.commit()
         return redirect(url_for("login"))
@@ -51,7 +52,7 @@ def login():
     if request.method == "POST":
         user = Users.query.filter_by(
             username=request.form.get("username")).first()
-        if user.password == request.form.get("password"):
+        if check_password_hash(user.password, request.form.get("password")):
             login_user(user)
             return redirect(url_for("home"))
     return render_template("login.html")
